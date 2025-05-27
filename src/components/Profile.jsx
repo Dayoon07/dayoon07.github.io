347441bc065ac65165534614812acc926045d1f4
Dayoon07 <gangd0642@gmail.com>
2025-05-28 03:46:48 +0900

    a

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -37,7 +37,7 @@ export default function Profile() {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
                                         <path d="M12 3L1 9L12 15L23 9L12 3ZM4.12 10.5L12 14.78L19.88 10.5L12 6.22L4.12 10.5ZM5 13V17H19V13L12 17L5 13Z"/>
                                     </svg>
-                                    <p>근명고등학교</p>
+                                    <p>근명고등학교</p> 
                                 </div>
                                 <div className="flex items-center space-x-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
