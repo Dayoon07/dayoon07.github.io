6677cacb2f1e819677b3a0fcf34413a9b3afd64f
Dayoon07 <gangd0642@gmail.com>
2025-05-28 02:58:04 +0900

    useEffect 의존성 힘들다

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -86,7 +86,7 @@ export default function Projects() {
         };
 
         preloadImages();
-    }, [l]);
+    }, [projectsData]);
 
     const openLightbox = (image) => {
         setCurrentImage(image);
